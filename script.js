ba16520f06ced83095486447a198ffabedc71557
Tyler <tylerlvvu@gmail.com>
2023-02-28 14:37:30 -0800

    updating

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var key = "13e05e959b2f9c3e9ce652c35b101c08"
 var city = "Seattle"
+var searchButtonEl = document.querySelector("#searchBtn")
+var searchInputEl = document.querySelector("#cityName")
 
 function getWeather (city){
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=` + city + `&appid=` + key)
@@ -12,7 +14,12 @@ function getWeather (city){
 }
 
 // add event listener for search button to grab value in input and set it to variable called city, then call getWeather passing city
-getWeather(city)
+searchButtonEl.addEventListener("click", function(event){
+    event.preventDefault()
+var search = searchInputEl.value.trim()
+getWeather(search)
+})
+
 
 // city name, date, icon, temperature, humidity, wind speed 
 
