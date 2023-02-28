dd3804fdb25bfd246b1c6e379d513d8494f9d5e2
Tyler <tylerlvvu@gmail.com>
2023-02-28 15:58:57 -0800

    adding date

--- script.js
+++ script.js
@@ -2,39 +2,44 @@ var key = "13e05e959b2f9c3e9ce652c35b101c08"
 var city = "Seattle"
 var searchButtonEl = document.querySelector("#searchBtn")
 var searchInputEl = document.querySelector("#cityName")
-var lat; 
+var lat;
 var lon;
 
-function getWeather (city){
+function getWeather(city) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=` + city + `&appid=` + key)
-    .then((res) => {
-        return res.json()
-    })
-    .then((data) => {
-        console.log(data)
-        lat=data.coord.lat;
-        lon=data.coord.lon;
-    getWeatherPlusForecast(city, lat, lon);
-    })
+        .then((res) => {
+            return res.json()
+        })
+        .then((data) => {
+            console.log(data)
+            lat = data.coord.lat;
+            lon = data.coord.lon;
+            getWeatherPlusForecast(city, lat, lon);
+        })
 }
-function getWeatherPlusForecast(city, lat, lon){
+function getWeatherPlusForecast(city, lat, lon) {
     fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}`)
-    .then((res) => {
-        return res.json()
-    })
-    .then((data) => {
-        console.log(data)
-    // getWeatherPlusForecast(city, lat, lon);
-    })
+        .then((res) => {
+            return res.json()
+        })
+        .then((data) => {
+            console.log(data)
+            displayWeather(data);
+        })
 }
-// add event listener for search button to grab value in input and set it to variable called city, then call getWeather passing city
-searchButtonEl.addEventListener("click", function(event){
+function displayWeather(data) {
+    let cityEl = document.getElementById("city");
+    let cityText = city;
+    let date = data.list[0].dt_txt.split(" ")[0];
+    let icon = `https://openweathermap.org/img/w/${data.list[0].weather[0].icon}.png`;
+    let desc = data.list[0].weather[0].description;
+
+        cityEl.textContent = `${cityText}  ${date}`;
+
+}
+searchButtonEl.addEventListener("click", function (event) {
     event.preventDefault()
-var search = searchInputEl.value.trim()
-city = search;
-getWeather(search)
+    var search = searchInputEl.value.trim()
+    city = search;
+    getWeather(search)
 })
-
-
-// city name, date, icon, temperature, humidity, wind speed 
-
